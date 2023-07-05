a7be06987e6aab7c80c70f8880d7a7cdb2f2a05c
Hamza Ammar <hamza.99.ammar@gmail.com>
2023-07-05 17:53:24 +0300

    addded correct query to the query suggestions list

--- query_refinement.py
+++ query_refinement.py
@@ -2,16 +2,18 @@ import shelve
 from collections import Counter
 
 import ir_datasets
+from nltk.corpus import words
+from spellchecker import SpellChecker
 
 _technology_queries = set()  # empty set
 _quora_queries = set()  # empty set
 
 
 def initialize_queries_db() -> None:
-    with shelve.open('db/' + "technology" + '_queries.db') as db:
+    with shelve.open('C:/Users/Hamza/Desktop/ir-search-engine/db/' + "technology" + '_queries.db') as db:
         db['queries'] = set()
 
-    with shelve.open('db/' + "quora" + '_queries.db') as db:
+    with shelve.open('C:/Users/Hamza/Desktop/ir-search-engine/db/' + "quora" + '_queries.db') as db:
         db['queries'] = set()
 
 
@@ -24,7 +26,7 @@ def index_query(query: str, dataset_name: str) -> None:
     else:
         _quora_queries.add(query)
 
-    with shelve.open('db/' + dataset_name + '_queries.db') as db:
+    with shelve.open('C:/Users/Hamza/Desktop/ir-search-engine/db/' + dataset_name + '_queries.db') as db:
         queries = db['queries']
         queries.add(query)
         db['queries'] = queries
@@ -42,7 +44,7 @@ def set_query_refinement_global_variables() -> None:
     forum_queries = set(query.text for query in forum_generic_queries)
     search_queries = set(query.text for query in search_generic_queries)
 
-    with shelve.open('db/' + "technology" + '_queries.db') as db:
+    with shelve.open('C:/Users/Hamza/Desktop/ir-search-engine/db/' + "technology" + '_queries.db') as db:
         stored_technology_queries = db['queries']
 
     _technology_queries = forum_queries.union(search_queries).union(stored_technology_queries)
@@ -51,7 +53,7 @@ def set_query_refinement_global_variables() -> None:
 
     quora_generic_queries = set(ir_datasets.load("beir/quora/test").queries_iter())
 
-    with shelve.open('db/' + "quora" + '_queries.db') as db:
+    with shelve.open('C:/Users/Hamza/Desktop/ir-search-engine/db/' + "quora" + '_queries.db') as db:
         stored_quora_queries = db['queries']
 
     _quora_queries = set(query.text for query in quora_generic_queries).union(stored_quora_queries)
@@ -84,9 +86,33 @@ def _get_ranked_suggestion(suggestions: list) -> list:
     return [suggestion[0] for suggestion in ranked_suggestions]
 
 
+def _suggest_corrected_query(text: str):
+    spell = SpellChecker()
+
+    word_set = set(words.words())
+
+    # Create a list to store the corrected tokens
+    corrected_tokens = []
+
+    # Spell check each token
+    for token in text.split():
+        if token in word_set:
+            corrected_tokens.append(token)
+        else:
+            suggestions = spell.candidates(token)
+            if suggestions:
+                corrected_tokens.append(spell.correction(token))
+            else:
+                corrected_tokens.append(token)
+
+    return ' '.join(corrected_tokens)
+
+
 def get_ranked_query_suggestions(query: str, dataset_name: str):
-    suggestions = _get_query_suggestions(query, dataset_name)
+    corrected_query = _suggest_corrected_query(query)
+    suggestions = _get_query_suggestions(corrected_query, dataset_name)
     ranked_suggestions = _get_ranked_suggestion(suggestions)
+    ranked_suggestions.insert(0, corrected_query)
     return ranked_suggestions[:15]
 
 
